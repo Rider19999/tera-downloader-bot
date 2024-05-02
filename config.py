f2b4ae699765270f5a7e903378b2a8723bac0fc1
Rider19999 <140139292+Rider19999@users.noreply.github.com>
2024-05-03 00:51:43 +0530

    Update config.py
    Yes

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ BOT_TOKEN = "6533528116:AAFTiURkV3ifi6sbXftYEhx5QOpYuYHDCT0"  # bot token
 
 
 ## REDIS
-HOST = "localhost"  # redis host uri
+HOST = "redis-15759.c241.us-east-1-4.ec2.redns.redis-cloud.com"  # redis host uri
 PORT = 6379  # redis port
-PASSWORD =    # redis password
+PASSWORD =  "FZTC7qGcXbFi88R4FIoJ2psJNAOrb27e"  # redis password
 
 PRIVATE_CHAT_ID = -1001823034357  # CHAT WHERE YOU WANT TO STORE VIDEOS
-COOKIE = "csrfToken=K7q0LKnyKeaz9j6YnnuAwgk9; browserid=TBBcIad85gSDEFiiX_2Sz-1plQqqfl1POXJLMdQz1MJEaQavQgispaMHP4k=; lang=en; TSID=h1mhgNtGPv7dCXz3xNVqZs0DrHgjWu13; __bid_n=18f163d54475c307924207; _ga=GA1.1.1466543190.1714065078; g_state={"i_l":0}; ndus=Yuy7MenteHuiZuVfDFAONlLzWWob0fiL8nLWmQDw; _ga_06ZNKL8C2E=GS1.1.1714065078.1.1.1714065391.60.0.0; ndut_fmt=7108F7140976389FFC4C48FB1B25B9953DF2A0A5E932D8459CA69A16CA612FEF"  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
-ADMINS = 1392460216
+COOKIE = """{'_ga': 'GA1.1.1211427720.1712288294', 'stripe_sid': 'd034eabe-581a-4ec0-8626-cff625660327b2ad09', 'g_state': '{"i_l":0}', 'bid_n': '18eac5c78d52769bd34207', 'browserid': 'FzoVyvGEWTZ4PqtIxXLHM5h1u0NPpT24GICAuQG_3JfxVvbqaWqG_fBAmnY=', '__stripe_mid': '41f26875-1bb0-4a4c-9e9d-325915a9152ca35c87', 'lang': 'en', '_ga_RSNVN63CM3': 'GS1.1.1712288293.1.0.1712288675.60.0.0', 'csrfToken': 'u149Lpp2VQHoAAbKNBsRLzwK', '_ga_06ZNKL8C2E': 'GS1.1.1713587860.8.1.1713587866.54.0.0', 'ndus': 'YyyTi3eteHuiGkmRmeY-2-fTOY0qMEuBM1cvIzCN', 'ndut_fmt': '65150E750CF7D95E2F3C5B91E0B2209C55C3B8D48E47032DD6B5834EA0C92D8B','ab_bid':'2e600299fec4c5699a488db87a44bf67de01','ab_jid':'652b2bd000aef41ee0ab25ce44682e600299','ab_sr':'1.0.1_OTY2MWI4YWQ2MzE0MzA4NTJmOTg4MjIwOTQ4OWM4MTMzNGM5YTg0MWU3OTcwZDFiZTc3ZDRkOWJmMmQwNGExNWZhZWQwODk5MmQwY2JlMWEwNWRjYjk3Y2QyMTRhZDUwY2UzYjY3OWRmN2FlYWExZTM3YmI4MzNiMTgxNjVlZWVjMTZlY2I2Y2U3OTQ1YjJjZWVmYTg3NmVhMTQzYzQzZQ=='}"""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
+ADMINS = [1392460216]
